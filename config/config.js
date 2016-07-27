310ab475d6e7aa90927bea664b09deced58e6dd8
abraham <abraham.paniagua@gmail.com>
2016-07-26 21:40:45 -0700

    Changed dev port to 3333

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
   development: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3333,
     db: process.env.DATABASE_URL || 'postgres://postgres@localhost/crema',
     logLevel: process.env.LOG_LEVEL || 'dev',
     secret: process.env.SECRET || 'Get Ready 4 a Sompop quiz!',
